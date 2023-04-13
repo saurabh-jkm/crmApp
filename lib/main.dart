751ccb8de7ece8166d67cfdcf0b5acc4060632de
Guddu Singh <128782863+guddusingh101@users.noreply.github.com>
2023-04-13 16:09:52 +0530

    jjjj

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ import 'screens/main/main_screen.dart';
 import 'package:firedart/firedart.dart';
 
 void main() async {
-  // // WidgetsFlutterBinding.ensureInitialized();
+  // // //WidgetsFlutterBinding.ensureInitialized();
   // if (kIsWeb) {
   //   await Firebase.initializeApp(
   //     options: FirebaseOptions(
